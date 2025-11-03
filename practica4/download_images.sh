327ab57d0140ca2bcd73200e8429ffc9f72fa44b
DevSlashRichie <me@slashrichie.dev>
2025-11-03 12:38:08 -0600

    feat: push

---
+++ practica4/download_images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download main character image
+curl -o personaje.png "https://static.wikia.nocookie.net/evangelion/images/0/08/Shinji_%28Plug_Suit%29.png/revision/latest?cb=20130429152013&path-prefix=es"
+
+# Download power-up images
+curl -o power-up-1.png "https://static.wikia.nocookie.net/evangelion/images/b/b1/Evangelion_Unit_01_versionTv.png/revision/latest?cb=20150924072426&path-prefix=es"
+curl -o power-up-2.png "https://static.wikia.nocookie.net/evangelion/images/4/43/Evangelion_LaUnit_02_%28EoE%29.png/revision/latest?cb=20150924115629&path-prefix=es"
+curl -o power-up-3.jpg "https://static.wikia.nocookie.net/evangelion/images/0/02/PeliRebuild30.jpg/revision/latest?cb=20151001200439&path-prefix=es"
+curl -o power-up-4.png "https://static.wikia.nocookie.net/evangelion/images/e/e2/Eva01_berserk.png/revision/latest?cb=20120312012451"
